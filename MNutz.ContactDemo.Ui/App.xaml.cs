924ae1b77dcd902b426ace5346bdbe02c9eb2652
Matthias Nutz <matthias@mnutz.de>
2024-08-07 11:56:40 +0200

    Add project files

---
+++ MNutz.ContactDemo.Ui/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace MNutz.ContactDemo.Ui
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
